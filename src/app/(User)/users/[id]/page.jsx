bd115c0c4f4408b77cf62f5a924056fbb6ed01e6
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-12-25 20:08:09 +0530

    Modify the metadata for the registered user

--- src/app/(User)/users/[id]/page.jsx
+++ src/app/(User)/users/[id]/page.jsx
@@ -12,7 +12,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-     <HeroSection/>
+    <HeroSection/>
+    
     <WhoWeAre/>
     <ProductCardUser/>
     <CategoriesSection/>
